f8f1fd710cb8680076c8db261332f1bc0d188784
iris-shade <98418356+iris-shade@users.noreply.github.com>
2024-11-14 09:28:52 +0530

    Add files via upload

---
+++ app/page_master.sql
@@ -0,0 +1,8 @@
+INSERT into page_master (name,value) values
+("carousel_one","https://dummyimage.com/600x400/000/fff&text=Image+1"),
+("carousel_two","https://dummyimage.com/600x400/000/fff&text=Image+2"),
+("carousel_three","https://dummyimage.com/600x400/000/fff&text=Image+3"),
+("about_us","https://dummyimage.com/600x400/000/fff&text=About+Us"),
+("team_member_one","https://dummyimage.com/600x400/000/fff&text=Team+1"),
+("team_member_two","https://dummyimage.com/600x400/000/fff&text=Team+2"),
+("team_member_three","https://dummyimage.com/600x400/000/fff&text=Team+3");
